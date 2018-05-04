b91737dcaabbe49ae7cc0659375fe39169703528
Alan Killian <akillian@scarsdaleschools.org>
2018-05-04 11:46:08 -0400

    Add Password protect Answer Key

---
+++ index.js
@@ -0,0 +1,8 @@
+// js includes
+const passwd = require('./src/js/main');
+
+passwd();
+
+// if you want to add more js libs, just put the require statements here
+
+// if you want add CSS libs, you're going to have to use a Browserify CSS transform like say Browserify-CSS
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,15 @@
 // DOM is ready
-$(document).ready(function () {
-    let moreLink = document.createElement("a");
-    let lessLink = document.createElement("a");
-    let allParagraphs = document.getElementsByTagName("p");
-    let firstParagraph = allParagraphs[0];
-
-    moreLink.setAttribute("href", "#");
-    moreLink.setAttribute("class", "more-link");
-    moreLink.innerHTML = "Read more";
-
-    firstParagraph.appendChild(moreLink);
-
-    for (let p of allParagraphs) {
-        if (p != allParagraphs[0]) {
-            p.style.display = "none";
-        }
+const passwd = function() {
+  const el = document.querySelector('[href="#"]');
+  el.addEventListener("click", function() {
+    const password = prompt("Enter the password to download the file:");
+    if (password != null && password.toLowerCase() === "writecode") {
+      location.assign("answerKey.txt");
+    } else {
+      alert("incorrect password!! please try again");
+      location.reload(); 
     }
+  });
+};
 
-
-    // ============ Routine to reveal hidden paragraphs ======== //
-
-    $(".more-link").on("click", function () {
-        let lastParagraph = allParagraphs[allParagraphs.length - 1];
-        moreLink.style.display = "none";
-
-        for (let p of allParagraphs) {
-            if (p != allParagraphs[0]) {
-                p.style.display = "block";
-            }
-        }
-
-        lessLink.setAttribute("href", "#");
-        lessLink.setAttribute("class", "less-link");
-        lessLink.innerHTML = "Less";
-        lastParagraph.appendChild(lessLink);
-
-        if (lessLink.style.display === 'none') {
-            lessLink.style.display = "inline-block";
-        }
-
-    });
-
-
-    // ====== Routine to hide paragraphs ============= //
-    // note: we had to handle this with event delegation ==== //
-
-    $("p").on("click", ".less-link", function () {
-        console.log("Clicked less-link button");
-        lessLink.style.display = "none";
-
-        for (let p of allParagraphs) {
-            if (p != allParagraphs[0]) {
-                p.style.display = "none";
-            }
-        }
-
-        moreLink.style.display = "inline-block";
-    });
-
-
-}); // end dom ready
+module.exports = passwd;
